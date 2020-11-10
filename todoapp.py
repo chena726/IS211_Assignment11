c77a97486de51c8424166c57e0ac297c0b70455b
chena726 <70415179+chena726@users.noreply.github.com>
2020-11-09 20:02:32 -0500

    Add files via upload

---
+++ todoapp.py
@@ -0,0 +1,78 @@
+from flask import Flask, redirect, render_template,request
+import re
+import os.path
+from os import path
+
+#run(use_reloader=False)
+
+app = Flask(__name__)
+
+todo_list = []
+
+
+@app.route("/")
+def index():
+    todo_list.clear()
+    if not path.exists('todolist.txt'):
+        open("todolist.txt", "w+")
+    with open("todolist.txt", 'r') as tasks:
+        for task in tasks:
+            todo_list.append(tuple(task.split(',')))        
+         
+    return render_template('index.html',todo=todo_list)
+
+# new item
+@app.route("/submit", methods=['POST'])
+def submit():
+    todo_list.clear()
+    task = request.form['task']
+    email = request.form['email']
+    priority = request.form['priority']
+    
+    if not re.match(r'(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)', email):
+        return redirect("/")
+    elif not task:
+        return redirect("/")
+    elif priority == "Select Priority":
+        return redirect("/")
+    
+    f = open("todolist.txt", "a+")
+    f.write(task +','+ email + ','+ priority+ '\n')
+    f.close()
+
+    todo_list.append((task,email,priority))
+    print(todo_list)
+
+    return redirect('/')
+
+# Clear 
+@app.route("/clear", methods=['POST'])
+def clear():
+    f = open('todolist.txt', 'r+')
+    f.truncate(0)
+    return redirect("/")
+
+# Delete 
+@app.route("/delete", methods=['POST'])
+def delete():
+    todo_list.clear()
+    with open("todolist.txt", 'r') as tasks:
+        for task in tasks:
+            todo_list.append(tuple(task.split(',')))
+    f = open('todolist.txt', 'r+')
+    f.truncate(0)
+
+    id = request.form['deleteId']
+    del todo_list[int(id)-1]
+    
+    for item in todo_list:
+        f = open("todolist.txt", "a+")
+        f.write(item[0]+', '+item[1]+', '+ item[2])
+        f.close()
+
+    return redirect('/')
+    
+
+
+if __name__=='__main__':
+    app.run(debug=True)    
